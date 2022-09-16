07d82966b9d2d4adddcb6e80367f60726801fc20
HombreLaser <sebastian-440@live.com>
2022-09-15 21:35:03 -0500

    Añadir Autor al contexto de la bd

--- Models/LibraryContext.cs
+++ Models/LibraryContext.cs
@@ -5,7 +5,5 @@ namespace LibraryAPI.Models {
         public DbSet<Author>? Authors { get; set; }
         public DbSet<Book>? Books { get; set; }
         public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) {}
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //    => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=library;Username=luis");
     }
 }
