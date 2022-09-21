f20b0eec603e8951cf78ed119745a85fde521706
HombreLaser <sebastian-440@live.com>
2022-09-20 20:34:32 -0500

    Añadido un validador para el autor

--- Models/Author.cs
+++ Models/Author.cs
@@ -10,6 +10,7 @@ namespace LibraryAPI.Models {
         [StringLength(64, ErrorMessage = "Name too long")]
         public string? Name { get; set; }
         [Required]
+        [Range(0, 2020, ErrorMessage = "Year out of valid range")]
         public int YearOfBirth { get; set; }
         [Required]
         public string[]? languages { get; set; }
